edf336772adf0db84575fdc02e7380e922692f2c
Avellin003 <uavellinbonaparte@gmail.com>
2024-05-02 17:48:28 +0200

    task_5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """function that takes in a list of floats and returns floats"""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
-    return sum(input_list)
+def sum_list(input_list: List[float]) -> float:
+    """returns a float
+    """
+    return float(sum(input_list))
