763486bbff52d56d7e655f78ea73e2d1a55a949b
root <root@avellin.localdomain>
2024-05-02 18:50:31 +0200

    updates

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
+
 from typing import Iterable, List, Sequence, Tuple
-"""this function takes a list of sequences and returns a
+"""this function takes a list of sequences and returns
 list of tuples"""
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """returns a list of tuples
-    """
+    """returns a list of tuples"""
     return [(i, len(i)) for i in lst]
