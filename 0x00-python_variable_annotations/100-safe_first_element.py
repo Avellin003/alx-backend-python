2fe0f39f8f43e647ca18fd84ee11f347a600e7ac
Avellin003 <uavellinbonaparte@gmail.com>
2024-05-02 18:18:46 +0200

    update

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''task 100
+'''
+from typing import Any, Sequence, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    '''Gets the first element of a sequence
+    '''
+    if lst:
+        return lst[0]
+    else:
+        return None
+    
