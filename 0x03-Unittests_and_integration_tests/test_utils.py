176774a29466ecc6556605155439674524c14afd
Avellin003 <uavellinbonaparte@gmail.com>
2024-05-27 09:08:30 +0200

    new_attempt

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
+"""Tests for utils.py."""
 import unittest
 from unittest.mock import patch
 from utils import memoize
 
 
 class TestMemoize(unittest.TestCase):
+    """Tests for the memoize decorator."""
     def test_memoize(self):
+        """Test that the memoize decorator caches the result of a method."""
         class TestClass:
+            """A class with a method and a memoized property."""
             def a_method(self):
                 return 42
 
@@ -20,8 +24,11 @@ class TestMemoize(unittest.TestCase):
                 return_value=lambda: 42,
                 ) as mock_method:
             test = TestClass()
+            # Call the method twice, but the method should only be called once.
             self.assertEqual(test.a_property(), 42)
+            # The method should not be called again.
             self.assertEqual(test.a_property(), 42)
+            # The method should only be called once.
             mock_method.assert_called_once()
 
 
