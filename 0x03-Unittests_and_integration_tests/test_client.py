1998b5d8a925921d5c255ba1b40665b7476224fb
Avellin003 <uavellinbonaparte@gmail.com>
2024-05-27 09:18:51 +0200

    updates

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Tests for the client module."""
 import unittest
 from unittest.mock import patch, PropertyMock
 from client import GithubOrgClient
@@ -19,13 +20,17 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_public_repos_url.return_value = (
                 "https://api.github.com/orgs/test_org/repos"
             )
+            # Test that the list of repositories is what we expect.
             client = GithubOrgClient("test_org")
-
+            # The result should be a list of the names of the repositories.
             result = client.public_repos()
+            # The expected result is a list of the names of the repositories.
             expected = ["repo1", "repo2"]
+            # The result should be equal to the expected result.
             self.assertEqual(result, expected)
-
+            # The mock should have been called once.
             mock_public_repos_url.assert_called_once()
+            # The mock should have been called once.
             mock_get_json.assert_called_once_with(
                 "https://api.github.com/orgs/test_org/repos"
             )
